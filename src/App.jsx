81ff652359d7f1aa7c41ad2600f00d9a733b54d1
David <davidtorresr96@gmail.com>
2023-06-15 13:55:44 -0500

    v3

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,41 @@ function App() {
     fetch('https://api.sheety.co/f3d41739f79e26e0bcca4c9d027df686/sivigilaRutinaria/cie10')
       .then((response) => { return response.json()})
       .then((enfermedades) => {
-        console.log(enfermedades);
+        console.log(enfermedades['cie10']);
         setEnfermedades(enfermedades);
       });
   }, []);
 
    return (
     <div>
-      <ul>
-        {enfermedades.map((item) => (
-          <li key={item.id}>{item.capitulo}</li>
-        ))}
-      </ul>
+      
+      
+      <div>
+        {
+        Object.values(enfermedades['cie10']).map(item => 
+          <ul>
+          <li key={item.id}>{item.códigoDeLaCie10CuatroCaracteres}</li>
+          </ul>
+        )}
+        </div> 
+         
+        <ul>
+          
+
+{ Object.keys(enfermedades).forEach((e) => { 
+
+  console.log(e,':', enfermedades[e]);
+ 
+  // <ul>
+  //   <p>{id}</p>
+  // <li key={item.id}>{item.códigoDeLaCie10CuatroCaracteres}</li>
+  // </ul>
+  
+ })}
+
+</ul>
+
+      
     </div>
   );
 }
