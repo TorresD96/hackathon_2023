f84bc07b1969e96474db8a1e905046d7ee3b5a7e
David <davidtorresr96@gmail.com>
2023-06-15 16:51:02 -0500

    v4

--- src/App.jsx
+++ src/App.jsx
@@ -5,46 +5,52 @@ function App() {
   const [enfermedades, setEnfermedades] = useState([]);
 
   useEffect(() => {
+    
+
     fetch('https://api.sheety.co/f3d41739f79e26e0bcca4c9d027df686/sivigilaRutinaria/cie10')
       .then((response) => { return response.json()})
       .then((enfermedades) => {
+
         console.log(enfermedades['cie10']);
-        setEnfermedades(enfermedades);
-      });
-  }, []);
+        
+        setEnfermedades(enfermedades['cie10'])
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      })
+
+ }, []);
 
    return (
-    <div>
-      
-      
-      <div>
-        {
-        Object.values(enfermedades['cie10']).map(item => 
-          <ul>
-          <li key={item.id}>{item.códigoDeLaCie10CuatroCaracteres}</li>
-          </ul>
-        )}
-        </div> 
+   
+    <div className='cuerpo'>
+      <div className='logo'>
+        <h2>
+        DiseaseDetectives
+        </h2>
+        <div className='logo2'>
+
+        </div>
+      </div >
+   <ul>
+         {
+           enfermedades['cie10'] && (
+        (enfermedades['cie10']).map(item => 
          
-        <ul>
-          
-
-{ Object.keys(enfermedades).forEach((e) => { 
-
-  console.log(e,':', enfermedades[e]);
- 
-  // <ul>
-  //   <p>{id}</p>
-  // <li key={item.id}>{item.códigoDeLaCie10CuatroCaracteres}</li>
-  // </ul>
-  
- })}
-
-</ul>
-
+          <li key={item.id}>{item.id}</li>
+         
+        ))} 
+     
+     </ul> 
+     <div className='grafica'>
+     <iframe src="https://app.powerbi.com/view?r=eyJrIjoiMDUyYWJhZjUtYWNmZi00ODU1LWFhMWMtMDE1OTgyNjg2YTViIiwidCI6IjU3N2ZjMWQ4LTA5MjItNDU4ZS04N2JmLWVjNGY0NTVlYjYwMCIsImMiOjR9"></iframe></div> 
       
-    </div>
-  );
+     </div> 
+         
+   
+
+    
+ );
 }
 
 export default App;
